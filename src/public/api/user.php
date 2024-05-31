9e6a8d70d071d3434fbb369f0f44d1625829cd2d
Vann Liu <vann@cpcp.tw>
2024-05-31 11:29:56 +0800

    feat: api update

--- src/public/api/user.php
+++ src/public/api/user.php
@@ -116,6 +116,18 @@ switch ($method) {
           'data'=> [
             'user'=> [ // 任何互動的 api，如果需要更新 user，就增加 'data['user']' 這個欄位，內部結構和 GET user 的一摸一樣，前端接到就會複寫
               'votes'=> 15,
+              // ... 其他需要複寫 user 的欄位
+            ]
+          ]
+        ];
+      break;
+      case 'vote':
+        $res = [
+          'success'=> true,
+          'data'=> [
+            'user'=> [ // 任何互動的 api，如果需要更新 user，就增加 'data['user']' 這個欄位，內部結構和 GET user 的一摸一樣，前端接到就會複寫
+              'votes'=> 10,
+              // ... 其他需要複寫 user 的欄位
             ]
           ]
         ];
