084dab2a558ebd296f8a619f4c7fd84ee93f1316
Vann Liu <vann@cpcp.tw>
2024-05-31 21:13:10 +0800

    feat: api update

--- src/public/api/user.php
+++ src/public/api/user.php
@@ -7,6 +7,8 @@ $res = null;
 
 $t = $_GET['t'] ?? null;
 if( $t !== '$2y$12$sQkktWRb4MPjGBVhWL64fuxpFSCzfwBypHF3MeEYCf15xS5DCCdjO' ){
+  // 如果登入失效，回傳 'not login'
+  $res = 'not login';
   echo json_encode($res);
   exit;
 }
@@ -96,6 +98,7 @@ switch ($method) {
 
   case 'POST':
     $action = $_REQUEST['action'] ?? null;
+    $res = $action;
     switch ($action) {
       case 'addFav': // 加入最愛
         if( !empty($_REQUEST['name']) ){
@@ -103,6 +106,7 @@ switch ($method) {
             'success'=> true
           ];
         }
+      break;
       case 'removeFav': // 移除最愛
         if( !empty($_REQUEST['name']) ){
           $res = [
@@ -124,7 +128,8 @@ switch ($method) {
       case 'vote':
         if( !empty($_REQUEST['name']) && !empty($_REQUEST['votes']) ){
           $res = [
-            'success'=> true,
+            'success'=> false,
+            'code'=> 1, // 投票結果回應彈窗，1:下輪即將啟動，2:沒雙重驗證，3:票數不足，4:其他原因失敗，總之就是失敗
             'data'=> [
               'user'=> [ // 任何互動的 api，如果需要更新 user，就增加 'data['user']' 這個欄位，內部結構和 GET user 的一摸一樣，前端接到就會複寫
                 'votes'=> 10,
